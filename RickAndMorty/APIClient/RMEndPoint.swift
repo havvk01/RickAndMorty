7fb40a1e3b9e0f1d30f3c6bdb40ea80855db8d3f
havvk01 <havvk01@gmail.com>
2023-01-16 21:23:19 +0300

    - Added docstrings and API related files

---
+++ RickAndMorty/APIClient/RMEndPoint.swift
@@ -0,0 +1,18 @@
+//
+//  RMEndPoint.swift
+//  RickAndMorty
+//
+//  Created by Slava Havvk on 16.01.2023.
+//
+
+import Foundation
+
+/// Respresents unique API Endpoint
+@frozen enum RMEndPoint: String {
+    /// Endpoint to get character info
+    case character // "character"
+    /// Endpoint to get location info
+    case location
+    /// Endpoint to get episode info
+    case episode
+}
---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Slava Havvk on 16.01.2023.
+//
+
+import Foundation
+
+/// Object that represents a singlet API call
+final class RMRequest {
+    
+}
