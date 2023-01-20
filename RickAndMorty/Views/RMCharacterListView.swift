dcf288aeebda3b0148f86ee6cd9cad403d26487a
havvk01 <90330806+havvk01@users.noreply.github.com>
2023-01-20 14:13:50 +0300

    -- Added Showing Characters

--- RickAndMorty/Views/RMCharacterListView.swift
+++ RickAndMorty/Views/RMCharacterListView.swift
@@ -39,6 +39,7 @@ final class RMCharacterListView: UIView {
         addSubviews(collectionView, spinner)
         addConstraints()
         spinner.startAnimating()
+        viewModel.delegate = self
         viewModel.fetchCharacters()
         setUpCollectionView()
     }
@@ -65,15 +66,26 @@ final class RMCharacterListView: UIView {
         collectionView.dataSource = viewModel
         collectionView.delegate = viewModel
         
-        DispatchQueue.main.asyncAfter(deadline: .now()+2, execute: {
-            self.spinner.stopAnimating()
-            
-            self.collectionView.isHidden = false
-            
-            UIView.animate(withDuration: 0.4) {
-                self.collectionView.alpha = 1 
-            }
-        })
+//        DispatchQueue.main.asyncAfter(deadline: .now()+2, execute: {
+//            self.spinner.stopAnimating()
+//
+//            self.collectionView.isHidden = false
+//
+//            UIView.animate(withDuration: 0.4) {
+//                self.collectionView.alpha = 1
+//            }
+//        })
     }
     
+}
+
+extension RMCharacterListView: RMCharacterListViewViewModelDelegate {
+    func didLoadInitialCharacters() {
+        spinner.stopAnimating()
+        collectionView.isHidden = false
+        collectionView.reloadData() // Initial fetch
+        UIView.animate(withDuration: 0.4) {
+            self.collectionView.alpha = 1
+        }
+    }
 }
