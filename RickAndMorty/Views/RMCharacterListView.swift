2263d9cb534b8cc2ad5a7cc4d030f4e186b11eef
havvk01 <90330806+havvk01@users.noreply.github.com>
2023-01-22 19:13:42 +0300

    -- Added pagination indicator

--- RickAndMorty/Views/RMCharacterListView.swift
+++ RickAndMorty/Views/RMCharacterListView.swift
@@ -34,6 +34,8 @@ final class RMCharacterListView: UIView {
         collectionView.alpha = 0
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         collectionView.register(RMCharacterCollectionViewCell.self, forCellWithReuseIdentifier: RMCharacterCollectionViewCell.cellIdetnifier)
+        
+        collectionView.register(RMFooterLoadingCollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: RMFooterLoadingCollectionReusableView.idetifier)
         return collectionView
     }()
     
