cef87c0930f1679975d0f1038db5818a5a3a697b
havvk01 <90330806+havvk01@users.noreply.github.com>
2023-01-25 20:30:54 +0300

    -- Added Character deal view

--- RickAndMorty/ViewModels/RMCharacterDetailViewViewModel.swift
+++ RickAndMorty/ViewModels/RMCharacterDetailViewViewModel.swift
@@ -14,8 +14,14 @@ final class RMCharacterDetailViewViewModel {
         self.character = character
     }
     
+    public var requestUrl: URL? {
+        return URL(string: character.url)
+    }
+    
     public var title: String {
         character.name.uppercased()
     }
+    
+
 }
 
--- RickAndMorty/Views/RMCharacterDetailView.swift
+++ RickAndMorty/Views/RMCharacterDetailView.swift
@@ -7,8 +7,62 @@
 
 import UIKit
 
-class RMCharacterDetailView: UIView {
+/// View for single character info
+final class RMCharacterDetailView: UIView {
 
+    private var collectionView: UICollectionView?
+    
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .large)
+        spinner.hidesWhenStopped = true
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        return spinner
+    }()
+    
+    // MARK: -Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .systemPurple
+        let collectionView = createCollectionView()
+        self.collectionView = collectionView
+        addSubviews(collectionView, spinner)
+        addConstrains()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("unsupported")
+    }
 
-
+    private func addConstrains() {
+        guard let collectionView = collectionView else {
+            return
+        }
+        NSLayoutConstraint.activate([
+            spinner.widthAnchor.constraint(equalToConstant: 100),
+            spinner.heightAnchor.constraint(equalToConstant: 100),
+            spinner.centerXAnchor.constraint(equalTo: centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: centerYAnchor),
+            
+            collectionView.topAnchor.constraint(equalTo: topAnchor),
+            collectionView.leftAnchor.constraint(equalTo: leftAnchor),
+            collectionView.rightAnchor.constraint(equalTo: rightAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+    
+    private func createCollectionView() -> UICollectionView {
+        let layout = UICollectionViewCompositionalLayout { sectionIndex, _ in
+            return self.createSection(for: sectionIndex)
+        }
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(UICollectionView.self, forCellWithReuseIdentifier: "cell")
+        return collectionView
+    }
+    
+    private func createSection(for sectionIndex: Int) -> NSCollectionLayoutSection {
+        
+    }
+    
 }
