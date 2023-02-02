52572318326e12fb476491a189fb3e7b12ef9ba7
havvk01 <90330806+havvk01@users.noreply.github.com>
2023-02-02 21:06:45 +0300

    -- Episode Tab

---
+++ RickAndMorty/Views/Episode/RMEpisodeListView.swift
@@ -0,0 +1,100 @@
+//
+//  RMEpisodeListView.swift
+//  RickAndMorty
+//
+//  Created by Slava Havvk on 02.02.2023.
+//
+
+import UIKit
+
+protocol RMEpisodeListViewDelegate: AnyObject {
+    func rmEpisodeListView(_ characterListView: RMEpisodeListView, didSelectEpisode episode: RMEpisode)
+}
+
+/// View that handles showing list of episodes, loader, etc.
+final class RMEpisodeListView: UIView {
+    
+    public weak var delegate: RMEpisodeListViewDelegate?
+    
+    private let viewModel = RMCharacterListViewViewModel()
+    
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .large)
+        spinner.hidesWhenStopped = true
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        return spinner
+    }()
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 10, bottom: 10, right: 10)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.isHidden = true
+        collectionView.alpha = 0
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(RMCharacterCollectionViewCell.self, forCellWithReuseIdentifier: RMCharacterCollectionViewCell.cellIdetnifier)
+        
+        collectionView.register(RMFooterLoadingCollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: RMFooterLoadingCollectionReusableView.idetifier)
+        return collectionView
+    }()
+    
+    //MARK: Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        translatesAutoresizingMaskIntoConstraints = false
+        addSubviews(collectionView, spinner)
+        addConstraints()
+        spinner.startAnimating()
+        viewModel.delegate = self
+        viewModel.fetchCharacters()
+        setUpCollectionView()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("Unsupported")
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            spinner.widthAnchor.constraint(equalToConstant: 100),
+            spinner.heightAnchor.constraint(equalToConstant: 100),
+            spinner.centerXAnchor.constraint(equalTo: centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: centerYAnchor),
+            
+            collectionView.topAnchor.constraint(equalTo: topAnchor),
+            collectionView.leftAnchor.constraint(equalTo: leftAnchor),
+            collectionView.rightAnchor.constraint(equalTo: rightAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+    
+    private func setUpCollectionView() {
+        collectionView.dataSource = viewModel
+        collectionView.delegate = viewModel
+    }
+    
+}
+
+extension RMEpisodeListView: RMEpisodeListViewViewModelDelegate {
+      
+    func didSelectCharacter(_ character: RMCharacter) {
+        delegate?.rmCharacterListView(self, didSelectCharacter: character)
+    }
+    
+    func didLoadInitialCharacters() {
+        spinner.stopAnimating()
+        collectionView.isHidden = false
+        collectionView.reloadData() // Initial fetch
+        UIView.animate(withDuration: 0.4) {
+            self.collectionView.alpha = 1
+        }
+    }
+    
+    func didLoadMoreCharacters(with newIndexPath: [IndexPath]) {
+        collectionView.performBatchUpdates {
+            self.collectionView.insertItems(at: newIndexPath)
+        }
+    }
+}
---
+++ RickAndMorty/Views/RMEpisodeDetailView.swift
@@ -0,0 +1,21 @@
+//
+//  RMEpisodeDetailView.swift
+//  RickAndMorty
+//
+//  Created by Slava Havvk on 01.02.2023.
+//
+
+import UIKit
+
+final class RMEpisodeDetailView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Unsupported")
+    }
+    
+    
+}
