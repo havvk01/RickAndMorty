e5f0daae4ff72303de47ba4ea5b15ce7ffdc314f
havvk01 <90330806+havvk01@users.noreply.github.com>
2023-02-04 17:40:06 +0300

    -- Episode Detail View

--- RickAndMorty/Controllers/Other/RMEpisodeDetailViewController.swift
+++ RickAndMorty/Controllers/Other/RMEpisodeDetailViewController.swift
@@ -8,8 +8,8 @@
 import UIKit
 
 /// VC to show details about single episode
-final class RMEpisodeDetailViewController: UIViewController {
-
+final class RMEpisodeDetailViewController: UIViewController, RMEpisodeDetailViewViewModelDelegate {
+  
     private let viewModel: RMEpisodeDetailViewViewModel
     
     private let detailView = RMEpisodeDetailView()
@@ -17,7 +17,7 @@ final class RMEpisodeDetailViewController: UIViewController {
     // MARK: - Init
     
     init(url: URL?) {
-        self.viewModel = .init(endpointUrl: url)
+        self.viewModel = RMEpisodeDetailViewViewModel(endpointUrl: url)
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -32,8 +32,10 @@ final class RMEpisodeDetailViewController: UIViewController {
         view.addSubview(detailView)
         addConstraints()
         title = "Episodes"
-        
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(didTapShare))
+        
+        viewModel.delegate = self
+        viewModel.fetchEpisodeData()
     }
     
     private func addConstraints() {
@@ -49,6 +51,11 @@ final class RMEpisodeDetailViewController: UIViewController {
     private func didTapShare() {
         
     }
-
+    
+    // MARK: - Delegate
+    
+    func didFetchEpisodeDetails() {
+        detailView.configure(with: viewModel)
+    }
 
 }
--- RickAndMorty/Views/RMEpisodeDetailView.swift
+++ RickAndMorty/Views/RMEpisodeDetailView.swift
@@ -9,15 +9,44 @@ import UIKit
 
 final class RMEpisodeDetailView: UIView {
 
+    private var viewModel: RMEpisodeDetailViewViewModel?
+    
+    private var collectionView: UICollectionView?
+    
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView()
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        return spinner
+    }()
+    
+    // MARK: - Init
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = .systemRed
+        self.collectionView = createCollectionView()
+        addContrains()
     }
     
     required init?(coder: NSCoder) {
         fatalError("Unsupported")
     }
     
+    private func addContrains() {
+        NSLayoutConstraint.activate([
+        
+        ])
+    }
+    
+    private func createCollectionView() -> UICollectionView {
+        
+    }
+    
+    // MARK: - Public
+    
+    public func configure(with viewModel: RMEpisodeDetailViewViewModel) {
+        self.viewModel = viewModel
+    }
     
 }
