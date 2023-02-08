791c83b87d79c692989b1a1c391da5e459221298
havvk01 <90330806+havvk01@users.noreply.github.com>
2023-02-08 22:19:33 +0300

    -- Setting ViewModels

---
+++ RickAndMorty/ViewModels/Settings/RMSettingsViewViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  RMSettingsViewViewModel.swift
+//  RickAndMorty
+//
+//  Created by Slava Havvk on 08.02.2023.
+//
+
+import Foundation
+struct RMSettingsViewViewModel {
+    let cellViewModels: [RMSettingsCellViewModel]
+
+}
